50257433a980a99e5d2e71196afa81a7824df3c4
AminHSI <aminhsikpk@gmail.com>
2024-05-08 17:36:14 +0200

    bubble sort function demo added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <string>
 #include <array>
+#include <algorithm>
 using namespace std;
 
 /*
@@ -15,29 +16,46 @@ using namespace std;
 ○ Shell Sort
 */
 
+/*
+//copy(begin(numbers), end(numbers), begin(numbers2))
+*/
 
-
+//this determines the length of our list of numbers
 const int range = 1000;
 
 //this is a simple function to print our numbers in order
-void print(int(&numbers)[range]) {
-    
+void print(int(&numbers)[range]) {    
     for(int i=0 ; i<range ; i++) {
         cout << numbers[i] << endl;
     }
 }
 
-
 //this function generates random numbers in the specified range
 int randomnum() {
-
     random_device rand;
-
     uniform_int_distribution<int> num(0,range);
-
     return num(rand);
 }
 
+//first version of bubble sort function
+void bubblesort(int(&numbers)[range]) {
+
+    while(1) {
+        int counter = 1;
+        for(int i=0 ; i<(range-1) ; i++) {
+            if(numbers[i]>numbers[i+1]) {
+                int temp = numbers[i];
+                numbers[i] = numbers[i+1];
+                numbers[i+1] = temp;
+                counter = 0;
+            }
+        }
+        if (counter) break;
+    }
+}
+
+
+// M A I N
 //this is the main function where we create our random list of numbers and call functions to sort it 
 int main() {
 
@@ -46,7 +64,10 @@ int main() {
     for(int i=0 ; i<range ; i++) {
         numbers[i] = randomnum();
     }
-    
+
+    bubblesort(numbers);
+       
     print(numbers);
+
 }
 
